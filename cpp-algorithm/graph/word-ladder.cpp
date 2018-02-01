85bde5839a6a9e410c4d56150f5bce927dfc80fd
Dancy Xia <dancyxia@sophos.com>
2018-02-01 13:45:23 -0800

    [LEETCODE-126-word-ladder] simplify the algorithm

--- cpp-algorithm/graph/word-ladder.cpp
+++ cpp-algorithm/graph/word-ladder.cpp
@@ -1,207 +1,140 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <string>
 #include <queue>
-#include <stdexcept>
 #include <iostream>
 #include <unordered_set>
 #include <unordered_map>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
-        typedef struct _parent_node parent_node;
-        struct _parent_node{
-            string link_word;
-            parent_node *next;
-        };
-
-        typedef struct {
-            parent_node *parent;
-            int level;
-        } word_node;
 class WordLadder {
     public:
-        void free_res() {
-            for(auto it = discovered.begin(); it != discovered.end(); ++it) {
-                parent_node *parent = ((word_node*)it->second)->parent;
-                parent_node *temp = parent;
-                while (parent) {
-                    parent = parent->next;
-                    delete temp;
-                    temp = parent;
-                }
-                delete it->second;
-            }
-            discovered.clear();
-            //q = {};
-            dict.clear();
-        }
+        vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+            queue<string> q;
+            unordered_map<string, int> level;
+            unordered_map<string, vector<string>> parents;
+            unordered_set<string> dict;
 
-        vector<vector<string>> findLadders(string beginW, string endW, vector<string>& wordList) {
-            beginWord = beginW;
-            endWord = endW;
-            WORD_LEN = beginWord.length();
-            discovered.clear();
-            //q = {};
-            dict.clear();
-            for (string word: wordList) {
-                dict.insert(word);
+            for (string w: wordList) {
+                dict.insert(w);
             }
             dict.insert(endWord);
-            word_node *begin_node = new word_node;
-            begin_node->level = 0;
-            begin_node->parent = NULL;
-            discovered[beginWord] = begin_node;
+
+            level[beginWord] = 0;
             q.push(beginWord);
 
+            bool found = false;
             while (!q.empty()) {
-                string w = q.front();
+                string word = q.front();
                 q.pop();
-                if (discovered[w]->level >= max_level) {
-                    break;
-                } else {
-                    nav_linked_words(w);
-                }
-            }
-
-            cout << "start get path" << endl;
-            vector<vector<string> > ret = get_paths(endWord);
-            free_res();
-            return ret;
-        }
-    private:
-        queue<string> q;
-        unordered_map<string, word_node*> discovered;
-        unordered_set<string> dict;
-        int max_level = 9999;
-        string beginWord;
-        string endWord;
-        int WORD_LEN;
-
-        bool match(string& a, string& w) {
-            bool diff_found = false;
-            for (int i = 0; i < a.length(); i++) {
-                if (w[i] != a[i]) {
-                    if(diff_found) {
-                        return false;
-                    }
-                    diff_found = true;
-                }
-            }
-
-            return true; //true only if there's only one difference
-        };
-
-        void nav_linked_words(string& word) {
-            if (dict.size() < 1) {
-            //  if (dict.size() < 26 * WORD_LEN) {
-                //iterate through dict
-                for(string key : dict)
-                {
-                    if (discovered.count(key) > 0 && discovered[key]->level <= discovered[word]->level) {
-                        continue;
-                    }
-                    if (match(key, word)) {
-                        word_node *node = discovered[key];
-                        if (!node) {
-                            node = new word_node;
-                            node->parent = NULL;
-                            discovered[key] = node;
-                        }
-                        node->level = discovered[word]->level + 1;
-                        parent_node *parent = new parent_node;
-                        parent->link_word = word;
-                        parent->next = node->parent;
-                        node->parent = parent;
-                        //discovered[key] = node;
-                        q.push(key);
-                        if (key == endWord) {
-                            max_level = node->level;
-                            //*q = {};
-                            //return;
-                        }
-                    }
-                }
-            } else {
-                string t = word;
-                for (int i = 0; i < WORD_LEN; i++) {
+                string tmp = word;
+                for (int i = 0; i < word.size(); i++) {
                     for (char c = 'a'; c <= 'z'; c++) {
-                        if (c != word[i]) {
-                            t[i] = c;
-                            //t is the parent or sibling node
-                            if (discovered.count(t) > 0 && discovered[t]->level <= discovered[word]->level) {
-                                continue; 
+                        if (c == word[i]) continue;
+
+                        tmp[i] = c;
+                        if (dict.find(tmp) != dict.end()) {
+                            if (level.count(tmp) > 0 && level[tmp] <= level[word] || (found && tmp != endWord)) {
+                                tmp[i] = word[i];
+                                continue;
                             }
-                            if (dict.find(t) != dict.end()) {
-                                word_node *node = discovered[t];
-                                if (!node) {
-                                    node = new word_node;
-                                    node->parent = NULL;
-                                    discovered[t] = node;
-                                }
-                                node->level = discovered[word]->level + 1;
-                                parent_node *parent = new parent_node;
-                                parent->link_word = word;
-                                parent->next = node->parent;
-                                node->parent = parent;
-                                //discovered[t] = node;
-                                q.push(t);
-                                if (t == endWord) {
-                                    max_level = node->level;
-                                    //*q = {};
-                                    //return;
+
+                            if (parents.count(tmp) == 0) {
+                                q.push(tmp);
+                                level[tmp] = level[word]+1;
+                                if (tmp == endWord) {
+                                    found = true;
                                 }
                             }
-                            t[i] = word[i];
+                            parents[tmp].push_back(word);
+                            cout << "get parent " <<word << " for " << tmp << endl;
                         }
+                        tmp[i] = word[i];
                     }
                 }
             }
-            };
 
-            vector<vector<string> > get_paths(string& word)
-            {
-                word_node *node = discovered[word];
-                parent_node *parent = node->parent;
-                vector<vector<string> > ret_paths;
-                if (!parent) {
-                    vector<string> path;
-                    path.push_back(word);
-                    ret_paths.push_back(path);
-                } else {
-                    while (parent) {
-                        vector<vector<string> > paths = get_paths(parent->link_word);
-                        for(vector<string> path: paths) {
-                            path.push_back(word);
-                            ret_paths.push_back(path);
-                        }
-                        parent = parent->next;
-                    }
-                }
-                return ret_paths;
-            };
+            vector<vector<string> > ret;
+            vector<string> path;
+            path.push_back(endWord);
+            getPaths(endWord, level, parents, path, ret);
+            return ret;
         };
+    private:
+        void getPaths(string& word, unordered_map<string, int>& level, unordered_map<string, vector<string> >& parent, vector<string>& path, vector<vector<string>>& ret)
+        {
+            if (level[word] == 0) {
+                reverse(path.begin(), path.end());
+                ret.push_back(path);
+                reverse(path.begin(), path.end());
+            } else {
+                for(int i = 0; i < parent[word].size(); i++) {
+                    path.push_back(parent[word][i]);
+                    getPaths(parent[word][i], level, parent, path, ret);
+                    path.pop_back();
+                }
+            }
+        };
+};
 
 int main(int argc, char **argv)
 {
+
+    //        bool match(string& a, string& w) {
+    //                bool diff_found = false;
+    //                for (int i = 0; i < a.length(); i++) {
+    //                if (w[i] != a[i]) {
+    //                    if(diff_found) {
+    //                        return false;
+    //                    }
+    //                    diff_found = true;
+    //                }
+    //            }
+    //
+    //            return true; //true only if there's only one difference
+    //        };
+    //
+
+    //                if (dict.size() < 1) {
+    //                //if (dict.size() < 26 * WORD_LEN) {
+    //                    //iterate through dict
+//                    for(string tmp : dict)
+//                    {
+//                        if (level.count(tmp) > 0 && level[tmp] <= level[word])
+//                            continue;
+//
+//                        if (match(tmp, word) && (!found || tmp == endWord)) {
+//                            parents[tmp].push_back(word);
+//                            if (level.count(tmp) == 0) {
+//                                level[tmp] = level[word]+1;
+//                                cout << "get parent " <<word << "(" << level[word] << ") for " << tmp << "(" << level[tmp] << ")" << endl;
+//                                q.push(tmp);
+//                                if (tmp == endWord) {
+//                                    found = true;
+//                                }
+//                            }
+//                        }
+//                    }
+//                } else {
+//
     string beginWord = "hit";
     string endWord = "cog";
-    vector<string> wordList = {"hot","dot","dog","lot","log","cog"};
+    vector<string> wordList = {"hot","dot","dog","lot","log","cog", "dit"};
     //Return
     //    [
     //    ["hit","hot","dot","dog","cog"],
     //    ["hit","hot","lot","log","cog"]
+    //    ["hit","dit","dot","dog","cog"]
     //        ]
     WordLadder ladder;
     vector<vector<string> > paths = ladder.findLadders(beginWord, endWord, wordList);
+    
     for (vector<string> path: paths) {
         for (string word : path) {
             cout << word << ", ";
         }
         cout << endl;
     }
-    //need to release the parent node
     return EXIT_SUCCESS;
 }
 
